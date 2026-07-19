ab2dd7127a776c143befccb5217e7ce76269b8cf
perrenduerinck-ctrl <perrenduerinck@gmail.com>
2026-07-18 18:29:08 -0700

    Create defaultSubclassNames.js

---
+++ defaultSubclassNames.js
@@ -0,0 +1,160 @@
+export const SUBCLASS_NAME_LIST = {
+  artificer: [
+    "Alchemist",
+    "Armorer",
+    "Artillerist",
+    "Battle Smith"
+  ],
+
+  barbarian: [
+    "Ancestral Guardian",
+    "Battlerager",
+    "Beast",
+    "Berserker",
+    "Giant",
+    "Storm Herald",
+    "Totem Warrior",
+    "Wild Magic",
+    "Zealot"
+  ],
+
+  bard: [
+    "Creation",
+    "Eloquence",
+    "Glamour",
+    "Lore",
+    "Spirits",
+    "Swords",
+    "Valor",
+    "Whispers"
+  ],
+
+  cleric: [
+    "Arcana",
+    "Death",
+    "Forge",
+    "Grave",
+    "Knowledge",
+    "Life",
+    "Light",
+    "Nature",
+    "Order",
+    "Peace",
+    "Tempest",
+    "Trickery",
+    "Twilight",
+    "War"
+  ],
+
+  druid: [
+    "Dreams",
+    "Land",
+    "Moon",
+    "Shepherd",
+    "Spores",
+    "Stars",
+    "Wildfire"
+  ],
+
+  fighter: [
+    "Arcane Archer",
+    "Banneret / Purple Dragon Knight",
+    "Battle Master",
+    "Cavalier",
+    "Champion",
+    "Echo Knight",
+    "Eldritch Knight",
+    "Psi Warrior",
+    "Rune Knight",
+    "Samurai"
+  ],
+
+  monk: [
+    "Ascendant Dragon",
+    "Astral Self",
+    "Drunken Master",
+    "Four Elements",
+    "Kensei",
+    "Long Death",
+    "Mercy",
+    "Open Hand",
+    "Shadow",
+    "Sun Soul"
+  ],
+
+  paladin: [
+    "Ancients",
+    "Conquest",
+    "Crown",
+    "Devotion",
+    "Glory",
+    "Oathbreaker",
+    "Redemption",
+    "Vengeance",
+    "Watchers"
+  ],
+
+  ranger: [
+    "Beast Master",
+    "Drakewarden",
+    "Fey Wanderer",
+    "Gloom Stalker",
+    "Horizon Walker",
+    "Hunter",
+    "Monster Slayer",
+    "Swarmkeeper"
+  ],
+
+  rogue: [
+    "Arcane Trickster",
+    "Assassin",
+    "Inquisitive",
+    "Mastermind",
+    "Phantom",
+    "Scout",
+    "Soulknife",
+    "Swashbuckler",
+    "Thief"
+  ],
+
+  sorcerer: [
+    "Aberrant Mind",
+    "Clockwork Soul",
+    "Divine Soul",
+    "Draconic Bloodline",
+    "Lunar Sorcery",
+    "Shadow Magic",
+    "Storm Sorcery",
+    "Wild Magic"
+  ],
+
+  warlock: [
+    "Archfey",
+    "Celestial",
+    "Fathomless",
+    "Fiend",
+    "Genie",
+    "Great Old One",
+    "Hexblade",
+    "Undead",
+    "Undying"
+  ],
+
+  wizard: [
+    "Abjuration",
+    "Bladesinging",
+    "Chronurgy",
+    "Conjuration",
+    "Divination",
+    "Enchantment",
+    "Evocation",
+    "Graviturgy",
+    "Illusion",
+    "Necromancy",
+    "Order of Scribes",
+    "Transmutation",
+    "War Magic"
+  ]
+};
+
+export default SUBCLASS_NAME_LIST;
